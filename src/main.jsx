b66328a9ba04e80d7d8c742707b25748a9bba898
Terry Lee <caiseye@gmail.com>
2025-05-15 17:08:15 +0900

    set up GitHub Actions for deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
-// main.jsx
-import React from 'react'; // ✅ 추가!
+import React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  // <StrictMode>
     <App />
-  </StrictMode>
+  // </StrictMode>
 );
